4eb7de264bf53e39d051598d8b32d520f5fd5100
Nazar Kharchuk <huntercat2785@gmail.com>
2022-08-20 13:24:12 +0300

    ..

--- src/functions/secondaryFunctions.js
+++ src/functions/secondaryFunctions.js
@@ -1,33 +1,5 @@
 import { useState, useEffect } from 'react';
 
-// export const useGetApi = () =>{
-//     const [requestData, setRequestData] = useState({
-//       isLoading: true,
-//       data:[],
-//       error: null
-//     });
-//     useEffect(() =>{
-//       fetch('https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json')
-//         .then(res => res.json())
-//         .then(
-//           (result) =>{
-//             setRequestData({
-//               isLoading: false,
-//               data: result,
-//               error:false
-//             })
-//           },
-//           (error) =>{
-//             setRequestData({
-//               isLoading: true,
-//               error:error
-//             })
-//           }
-//         )
-//     }, []);
-//     return requestData;
-//   }
-  
 
 export const useGetApi = () =>{
     const [requestData, setRequestData] = useState({
@@ -60,15 +32,6 @@ export const useGetApi = () =>{
     }, []);
     return requestData;
   }
-
-
-// export const getCurrencyPrice = (data) =>{
-//     let courseObject = {'UAH': 1,};
-//     data.forEach((item) =>{
-//       courseObject[item.cc] = item.rate;
-//     })
-//     return courseObject;
-//   } 
 
 export const roundingNumber = (number) =>{
     return Math.round(parseFloat(number) * 100) / 100;
